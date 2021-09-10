24436681acd200aa59eadcfbcfebbc3f2a67bb5b
CookDas2 <parksw125368@naver.com>
2021-09-11 01:20:22 +0900

    210911

--- app/src/main/java/com/example/a07_recorder/MainActivity.kt
+++ app/src/main/java/com/example/a07_recorder/MainActivity.kt
@@ -2,16 +2,23 @@ package com.example.a07_recorder
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
 
     private val recordButton : RecordButton by lazy {
         findViewById<RecordButton>(R.id.recordButton)
     }
     private val requiredPermissions = arrayOf(Manifest.permission.RECORD_AUDIO)
 
+    private val recordingFilePath :String by lazy {
+        "${externalCacheDir?.absolutePath}/recording.3gp"
+    }
+
+    private var recorder:MediaRecorder?= null
     private var state = State.BEFORE_RECORDING
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,6 +47,26 @@ class MainActivity : AppCompatActivity() {
     }
     private fun initViews(){
         recordButton.updateIconWithState(state)
+    }
+
+    private fun startRecording(){
+        recorder = MediaRecorder().run { //apply말고 한번 run의 개념 이해를 위해 run으로 시도해봄.
+            setAudioSource(MediaRecorder.AudioSource.MIC) //마이크
+            setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)//컨테이너 역할. 인코딩(압축)된 파일들을 차곡차곡 정리해둠
+            setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB)//AMR_NB방식으로 인코딩(버전에 상관없이 가능해서 AMR_NB방식을 체택)
+            setOutputFile(recordingFilePath)//저장할 경로
+            prepare() //세팅 다 끝내고 얘를 해줘야 녹음할 수 있는 상태가 됨.
+            this //run은 맨마지막 값을 반환하므로 this를 써줌.
+        }
+        recorder?.start() //실제 녹음 시작.
+    }
+
+    private fun stopRecording(){
+        recorder?.run {
+            stop()//녹음 멈춤.
+            release()//메모리 해제
+        }
+        recorder = null
     }
     companion object{ //자바의 상수와 같은 것
         private const val REQUEST_RECORD_AUDIO_PERMISSION = 201
